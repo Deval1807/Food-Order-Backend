f58196c3040c9f4d4f6a66deef83baab2cf6053f
Deval <deval135darji@gmail.com>
2024-06-04 11:59:51 +0530

    Add '/' route

--- src/services/ExpressApp.ts
+++ src/services/ExpressApp.ts
@@ -8,10 +8,14 @@ import { AdminRoute, ShoppingRoute, VendorRoute, CustomerRoute, DeliveryRoute } 
 export default async (app: Application) => {
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }))
-    
+
     const imagePath = path.join(__dirname,'../images');        
     app.use('/images', express.static(imagePath));
-        
+    
+    app.get('/', (req, res)=> {
+        res.send("Welcome to the Online Food-Order Backend")
+    })
+
     app.use('/admin', AdminRoute);
     app.use('/vendor', VendorRoute);
     app.use('/customer', CustomerRoute)
